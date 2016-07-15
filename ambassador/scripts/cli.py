0e5e450c1d5aa654803574217934a5781187685b
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-14 19:23:31 -0700

    Wait 3 API polls before sending API down notification

--- ambassador/scripts/cli.py
+++ ambassador/scripts/cli.py
@@ -12,40 +12,61 @@ import time
 import ambassador.settings
 
 import ambassador.cgc.ticlient
+import ambassador.cgc.tierror
 import ambassador.log
 from ambassador.notifier import Notifier
 from ambassador.retrievers.status import StatusRetriever
 
 LOG = ambassador.log.LOG.getChild('main')
-POLL_INTERVAL = int(os.environ.get('POLL_INTERVAL', 5))
+
+class CLI(object):
+    """A docstring"""
+    POLL_INTERVAL = int(os.environ.get('POLL_INTERVAL', 5))
+    NOTIFY_AFTER_NUMBER_OF_TRIES = 3
+
+    def __init__(self):
+        """Another docstring"""
+        # Initialize APIs
+        self.cgc = ambassador.cgc.ticlient.TiClient.from_env()
+        self.notifier = Notifier()
+        self.api_down_tries = 0
+
+    def api_down(self):
+        """Another docstring again"""
+        self.api_down_tries += 1
+        if self.api_down_tries == self.NOTIFY_AFTER_NUMBER_OF_TRIES:
+            self.notifier.api_is_down()
+
+    def api_up(self):
+        """And another"""
+        self.api_down_tries = 0
+        self.notifier.api_is_up()
+
+    def run(self):
+        """And another"""
+        while True:
+            try:
+                # wait for API to be available
+                while not self.cgc.ready():
+                    self.api_down()
+                    time.sleep(self.POLL_INTERVAL)
+
+                self.api_up()
+
+                status_retriever = StatusRetriever(self.cgc)
+                status_retriever.run()
+
+                LOG.info("Round #%d", status_retriever.current_round.num)
+
+                # FeedbackRetriever(self.cgc, _round).run()
+                # ConsensusEvaluationRetriever(self.cgc, _round).run()
+
+            except ambassador.cgc.tierror.TiError:
+                self.api_down()
+
+        return 0
 
 
 def main():
-    """Ambassador main body"""
-
-    # Initialize APIs
-    cgc = ambassador.cgc.ticlient.TiClient.from_env()
-    notifier = Notifier()
-
-    while True:
-        try:
-            # wait for API to be available
-            while not cgc.ready():
-                notifier.api_is_down()
-                LOG.debug("Sleeping for %d seconds", POLL_INTERVAL)
-                time.sleep(POLL_INTERVAL)
-
-            notifier.api_is_up()
-
-            status_retriever = StatusRetriever(cgc)
-            status_retriever.run()
-
-            LOG.info("Round #%d", status_retriever.current_round.num)
-
-            # FeedbackRetriever(cgc, _round).run()
-            # ConsensusEvaluationRetriever(cgc, _round).run()
-
-        except ambassador.cgc.tierror.TiError:
-            notifier.api_is_down()
-
-    return 0
+    """Oh shit"""
+    CLI().run()
