3b388dee5bac1731fc47f351869b900b88ba39a7
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-18 15:54:39 -0700

    Fixed CBSubmitter

--- ambassador/submitters/cb.py
+++ ambassador/submitters/cb.py
@@ -7,9 +7,7 @@ CBSubmitter module
 
 from __future__ import absolute_import
 
-from farnsworth.models import (ChallengeSetSubmissionCable,
-                               ChallengeSetFielding,
-                               IDSRuleFielding, Round, Team)
+from farnsworth.models import (ChallengeSet, Round)
 
 from ambassador.cgc.tierror import TiError
 import ambassador.log
@@ -26,39 +24,42 @@ class CBSubmitter(object):
         self._cgc = cgc
         self._current_round = current_round
 
+    def _submit_patches(self, cable):
+        patches = [(str(cbn.root.name), str(cbn.blob))
+                   for cbn in cable.cbns]
+        result = self._cgc.uploadRCB(str(cable.cs.name), *patches)
+        LOG.debug("Submitted RB! Response: %s", result)
+        submission_round, _ = Round.get_or_create(num=result['round'])
+        return cable.cs.submit(cbns=cable.cbns, round=submission_round)
+
+    def _submit_ids_rule(self, cable):
+        if cable.cbns and (cable.cbns[0].ids_rule is not None):
+            ids_rule = cable.cbns[0].ids_rule
+            result = self._cgc.uploadIDS(str(cable.cs.name), str(ids_rule.rules))
+            submission_round, _ = Round.get_or_create(num=result['round'])
+            return ids_rule.submit(round=submission_round)
+
     def run(self):
         """Amazing docstring"""
         # submit only in odd rounds, see FAQ163 & FAQ157
         if (self._current_round.num % 2) == 1:
 
-          for cable in ChallengeSetSubmissionCable.unprocessed():
-              patch = cable.cbns
-              original_cbid = patch.root.name
-              # FIXME!!!!
-              fixme = ChallengeSetFielding.select().where((ChallengeSetFielding.cs == cable.cbns.cs) & \
-                                                          (ChallengeSetFielding.submission_round == self._current_round) & \
-                                                          (ChallengeSetFielding.team == Team.get_our()))
-              if len(fixme) > 0:
-                  LOG.info("patch for CS already submitted in this round, move on...")
-                  break
+            for cs in ChallengeSet.fielded_in_round():
+                for cable in cs.unprocessed_submission_cables():
 
-              try:
-                  # submit rb
-                  result = self._cgc.uploadRCB(str(cable.cs.name),
-                                               (str(original_cbid),
-                                                str(patch.blob)))
-                  LOG.debug("Submitted RB! Response: %s", result)
-                  submission_round, _ = Round.get_or_create(num=result['round'])
-                  cable.cs.submit_patches(submission_round, patch)
-                  # submit ids
-                  if patch.ids_rule is not None:
-                      result = self._cgc.uploadIDS(str(cable.cs.name), str(patch.ids_rule.rules))
-                      submission_round, _ = Round.get_or_create(num=result['round'])
-                      IDSRuleFielding.get_or_create(ids_rule=patch.ids_rule,
-                                                    submission_round=submission_round,
-                                                    team=Team.get_our())
+                    try:
+                        patches_fielding = self._submit_patches(cable)
+                        ids_fielding = self._submit_ids_rule(cable)
+                        cable.process()
+                        if patches_fielding:
+                            LOG.info("Submitted %d RBs for %s in round %d",
+                                     patches_fielding.cbns.count(), cs.name,
+                                     patches_fielding.submission_round.num)
+                        if ids_fielding:
+                            LOG.info("Submitted IDS for %s in round %d",
+                                     cs.name, ids_fielding.submission_round.num)
+                    except TiError as err:
+                        LOG.error("RB Submission error: %s", err.message)
 
-              except TiError as err:
-                  LOG.error("RB Submission error: %s", err.message)
-
-              cable.process()
+                    # one CS submission per round
+                    break
