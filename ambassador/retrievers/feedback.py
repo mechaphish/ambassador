401e0946c9677b5e7ee350828e8636f87bdaac8f
Yan <yans@yancomm.net>
2016-07-22 00:02:31 -0700

    save feedback to feedback table

--- ambassador/retrievers/feedback.py
+++ ambassador/retrievers/feedback.py
@@ -8,7 +8,7 @@ FeedbackRetriever module
 from __future__ import absolute_import
 
 from farnsworth.models import (
-    Feedback,
+    Feedback, ChallengeSet, ChallengeSetFielding, PollFeedback, Team
 )
 
 from ambassador.cgc.tierror import TiError
@@ -38,7 +38,34 @@ class FeedbackRetriever(object):
 
     def run(self):
         """A run() method. Happy now pylint?"""
+        cgc_polls = self._get_feedback('poll')
         Feedback.update_or_create(self._round,
-                                  polls=self._get_feedback('poll'),
+                                  polls=cgc_polls,
                                   povs=self._get_feedback('pov'),
                                   cbs=self._get_feedback('cb'))
+
+
+        currently_fielded = { c.name: c for c in ChallengeSet.fielded_in_round(self._round) }
+        for c in cgc_polls:
+            cs = currently_fielded[c['csid']]
+
+            fielding = cs.fieldings.where(
+                ChallengeSetFielding.available_round == self._round,
+                ChallengeSetFielding.team == Team.get_our()
+            ).get()
+
+            if fielding.poll_feedback is not None:
+                continue
+
+            pf = PollFeedback.create(
+                cs=cs,
+                round_id=self._round.id,
+                success = float(c['functionality']['success'])/100.,
+                timeout = float(c['functionality']['timeout'])/100.,
+                connect = float(c['functionality']['connect'])/100.,
+                function = float(c['functionality']['function'])/100.,
+                time_overhead = float(c['performance']['time'])/100. - 1.,
+                memory_overhead = float(c['performance']['memory'])/100. - 1.,
+            )
+            fielding.poll_feedback = pf
+            fielding.save()
